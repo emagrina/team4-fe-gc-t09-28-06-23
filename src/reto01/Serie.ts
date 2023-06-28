d9219959a61d0a049d30e01edbfcfd8571cb0e3a
Ernest Magriñá <ernest8081@gmail.com>
2023-06-28 16:43:14 +0200

    feat: :package: Add parmeters

---
+++ src/reto01/Serie.ts
@@ -0,0 +1,12 @@
+const defaultNumeroTemporada = 3;
+const defaultEntregado = false;
+
+class Serie {
+  private titulo: string;
+  private numeroTemporada: number;
+  private entregaro: boolean;
+  private genero: string;
+  private creador: string;
+
+
+}
