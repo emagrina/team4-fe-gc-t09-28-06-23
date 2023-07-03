bb23ac8468e598dd9da9e505d1ab611cea24ac1a
Jorgyg <92818340+Jorgyg@users.noreply.github.com>
2023-07-03 18:21:13 +0200

    Update Cine.ts

--- reto05/src/Cine.ts
+++ reto05/src/Cine.ts
@@ -1,14 +1,18 @@
+//Declaramos las constantes con los vaores default
 const Pelicula="";
 const Precio=0;
 class Cine{
+    //Declaramos pelicula y precio
     protected Pelicula:string;
     protected Precio:number;
 
+    //Hacemos el contructor
     constructor(newPelicula:string,newPrecio:number){
         this.Pelicula=newPelicula;
         this.Precio=newPrecio;
     }
 
+    //Getters
     getPelicula():string{
         return this.Pelicula;
     }
