070d66f1f8306feb2dfefce8afd1c0a71562b5ea
Jorgyg <92818340+Jorgyg@users.noreply.github.com>
2023-07-03 18:22:04 +0200

    Update Espectador.ts

--- reto05/src/Espectador.ts
+++ reto05/src/Espectador.ts
@@ -1,8 +1,10 @@
 class Espectador extends Cine{
+    //Declaramos las variables
     private Nombre:string;
     private Edad:number;
     private Dinero:number;
 
+    //Creamos el constructor
     constructor(newNombre:string,newEdad:number,newDinero:number){
         super(Pelicula,Precio);
         this.Nombre=newNombre;
@@ -10,6 +12,7 @@ class Espectador extends Cine{
         this.Dinero=newDinero;
     }
 
+    //Getters
     getNombre():string{
         return this.Nombre;
     }
