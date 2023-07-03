8f7600c99d8199871ed5329bf39c2156a9421a2f
Jorgyg <92818340+Jorgyg@users.noreply.github.com>
2023-07-03 18:22:30 +0200

    Update Peliculas.ts

--- reto05/src/Peliculas.ts
+++ reto05/src/Peliculas.ts
@@ -1,9 +1,12 @@
 class Peliculas extends Cine{
+
+    //Declaramos las variables
     private Titulo:string;
     private Duracion:number;
     private EdadMinima:number;
     private Director:string;
 
+    //Creamos el constructor
     constructor(newTitulo:string,newDuracion:number,newEdadMinima:number,newDirector:string){
         super(Pelicula,Precio);
         this.Titulo=newTitulo;
@@ -12,6 +15,7 @@ class Peliculas extends Cine{
         this.Director=newDirector;
     }
 
+    //Getters
     getTitulo():string{
         return this.Titulo;
     }
